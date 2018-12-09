d0725b053e80c7727b73a097c0396c08ffb0c6b2
tonyaytlo <tonyaytlo@gmail.com>
2018-12-09 19:01:11 +0400

    selectable adapter un/select notify

--- app/src/main/java/com/tony/vkimage/presentation/adapter/SelectableAdapter.kt
+++ app/src/main/java/com/tony/vkimage/presentation/adapter/SelectableAdapter.kt
@@ -4,9 +4,11 @@ import android.support.v7.widget.RecyclerView
 
 abstract class SelectableAdapter<VH : RecyclerView.ViewHolder, I>(val data: MutableList<I>) : RecyclerView.Adapter<VH>() {
 
-//    companion object {
-//        private const val NO_ID
-//    }
+    companion object {
+        private const val NO_ID = -1
+        private const val PAYLOAD_UNSELECT = 0
+        private const val PAYLOAD_SELECT = 1
+    }
 
     private var selectedItem: I? = null
 
@@ -19,6 +21,12 @@ abstract class SelectableAdapter<VH : RecyclerView.ViewHolder, I>(val data: Muta
         var posPrev = RecyclerView.NO_POSITION
         if (selectedItem != null) {
             posPrev = data.indexOfFirst { it == selectedItem }
+        }
+        if (posPrev != RecyclerView.NO_POSITION) {
+            notifyItemChanged(posPrev, PAYLOAD_UNSELECT)
+        }
+        if (pos != NO_ID) {
+            notifyItemChanged(posPrev, PAYLOAD_SELECT)
         }
     }
 
